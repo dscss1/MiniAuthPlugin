7861f73a5acdc5ae257a5888d7a850c37204be61
dscss1 <86515441+dscss1@users.noreply.github.com>
2024-07-08 12:50:19 +0300

    Delete velocity directory

--- velocity/src/main/java/ua/dscss2/miniauthplugin/handler/FALLING_COORDS.java
+++
@@ -1,7 +0,0 @@
-package ua.dscss2.miniauthplugin.handler;
-
-class FALLING_COORDS {
-    public int X = 0;
-    public int Y = 512;
-    public int Z = 0;
-}
--- velocity/src/main/templates/ua/dscss2/miniauthplugin/BuildConstants.java
+++
@@ -1,7 +0,0 @@
-package ua.dscss2.miniauthplugin;
-
-// The constants are replaced before compilation
-public class BuildConstants {
-
-    public static final String VERSION = "${version}";
-}
