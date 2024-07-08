88025defa529b5891c5116fd12b9c9f872280eb9
dscss2 <euser6256@gmail.com>
2024-07-08 13:06:36 +0300

    Edit

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+group = "ua.dscss2.miniauthplugin"
+version = "1.0"
+
+ext["COMPILED_FOR"] = "SurvivalBoom Network 2024. Internal Use Only."
+
+val targetJava = 17
+
+plugins {
+    java
+}
+
+subprojects {
+
+    apply(plugin = "java")
+
+    version = rootProject.version
+    group = rootProject.group
+
+    tasks.withType(JavaCompile::class) {
+        options.release.set(targetJava)
+        options.encoding = "UTF-8"
+    }
+}
+
+dependencies {
+    subprojects.forEach { project: Project ->
+        implementation(project)
+    }
+}
+
+tasks.jar {
+
+    destinationDirectory.set(rootProject.projectDir)
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    subprojects.forEach { subproject ->
+        from(subproject.sourceSets.main.get().output)
+        subproject.configurations.runtimeClasspath.get().forEach { file ->
+            if (file.isDirectory) from(file)
+            else from(zipTree(file))
+        }
+    }
+
+    println("--- --- --- ---")
+    println(String.format("OUTPUT JAR PATH: %s", destinationDirectory.get().asFile.absolutePath))
+    println("--- --- --- ---")
+
+}
+
+tasks.withType(JavaCompile::class) {
+    options.release.set(targetJava)
+    options.encoding = "UTF-8"
+}
